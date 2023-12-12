63dd5baf6a9583038a87c0a7b036fca4861c5dff
yunju <psw05067@naver.com>
2023-12-12 22:31:43 +0900

    about fix

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-$(function () {});
+$(function () {
+  $(document).ready(function () {
+    $("#fullpage").fullpage({
+      navigation: true,
+      navigationPosition: "left",
+      afterLoad: function (anchorLink, index) {
+        if (index === 1) {
+          $(".head_wrap").addClass("active");
+        } else {
+          $(".head_wrap").removeClass("active");
+        }
+      },
+    });
+  });
+});
